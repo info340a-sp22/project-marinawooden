906ec7379545b7f7812fc2a98f3b5bab966ae701
Sam Lucia <slucia2@Heidis-MacBook-Pro.local>
2022-05-21 18:15:44 -0700

    Added a login feature and styling to the global css file

--- src/components/App.js
+++ src/components/App.js
@@ -3,11 +3,14 @@ import { SearchPage } from "./SearchPage";
 import 'bootstrap/dist/css/bootstrap.css';
 import { Routes, Route } from 'react-router-dom';
 import { ProfilePage } from "./ProfilePage";
+import { Login } from "./Login";
 
 
 export default function App(props) {
+    
     return (
         <Routes>
+            <Route path="login" element={<Login uname={props.uname}/>} />
             <Route path="profile" element={<ProfilePage />} />
             <Route path="*" element={
                 <SearchPage criteria={props.criteria} dataSet={props.dataSet} />
