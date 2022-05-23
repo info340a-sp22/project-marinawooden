f9becf6ba0f5a9a5bc07bd09d264f02601cb735c
jho0000 <jho0000@uw.edu>
2022-05-22 19:20:26 -0700

    Merging for myself

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 import React from "react";
 import { ReactDOM } from "react";
-import App from './components/App';
+
+//imported css
+import './index.css';
+import 'bootstrap/dist/css/bootstrap.css';
+
+//sources
+const picture = 'public/logo.png'
+const pic = <img src="picture" alt="MusicRoom logo"/>;
+//defined elements
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDzAvp6RaG98hREp-csQsOqmpo9kXw0k28",
+  authDomain: "musicroom-196ba.firebaseapp.com",
+  projectId: "musicroom-196ba",
+  storageBucket: "musicroom-196ba.appspot.com",
+  messagingSenderId: "122879506300",
+  appId: "1:122879506300:web:460255754098ebe341d6f9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+//render root
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(pic);
